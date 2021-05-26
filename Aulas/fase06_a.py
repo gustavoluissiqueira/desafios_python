7117553bd63f1041d59aa21cebdb6213cf24ee02
@gustavoluissiqueira <gustavoluissilvasiqueira@gmail.com>
2021-05-26 13:47:58 -0300

    Adicionado a pasta aula e o cófigo da aula 6

---
+++ Aulas/fase06_a.py
@@ -0,0 +1,22 @@
+#Tipos Primitivos e Saída de Dados
+
+#========================================================
+# int : 7 -4 0 9875
+# float : 4.5, 0.0007, 7.0, -15223
+# bool: True / False
+# str: 'Olá' '7.5' ''
+
+# print(f'A soma vale {x}')
+
+# n1 = (input('Digite um número:')
+# n2 = input('Digite mais um número:')
+# s = n1+n2
+
+# print(f'O resultado é {s}') Imprime 23, pois não utilizou os tipos primitivos 
+
+n1 = int(input("Digite um número:"))
+n2 = int(input('Digite mais um número:'))
+soma =  n1+n2
+
+print(f'O resultado é {soma}.')
+print(f'A soma entre {n1} + {n2} é igual a {soma}.')
