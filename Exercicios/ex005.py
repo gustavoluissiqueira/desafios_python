e224c4d33a11ae9e617ce31308e8bd936127f5c3
@gustavoluissiqueira <gustavoluissilvasiqueira@gmail.com>
2021-05-27 15:03:12 -0300

    Criação do Desafio (Exercício) 005

---
+++ Exercicios/ex005.py
@@ -0,0 +1,6 @@
+#Desafio 005 - Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
+num_int = int(input('Digite um número inteiro:'))
+antecessor = num_int - 1
+sucessor = num_int + 1
+print(f'Você digitou o número {num_int} e o seu antecessor é o número {antecessor} e seu sucessor é o número {sucessor}')
+
